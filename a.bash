e2aff0b9cecb5d93188ec54aac0fab64031a6ef9
prfigueroa <38894306+prfigueroa@users.noreply.github.com>
2020-07-14 13:15:02 -0700

    Create a.bash

---
+++ a.bash
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+NAMES=""
+NAMES="${NAMES} MYNAME"
+NAMES="${NAMES} YOURNAME"
+NAMES="${NAMES} THEIRNAME"
+
+echo 'One at a time...'
+for NAME in ${NAMES}; do
+    echo ${NAME};
+done
+
+echo 'All together now!'
+echo ${NAMES}
